542236fe254d453d9d2aca229ce313e78e757e4c
Jeremie <bj.06202000@gmail.com>
2024-04-25 21:41:07 +0200

    Start developing manage expense sheet functionality

--- app/src/main/java/com/example/gsb_mobile_app/ManageExpenseSheet.java
+++ app/src/main/java/com/example/gsb_mobile_app/ManageExpenseSheet.java
@@ -1,4 +1,41 @@
 package com.example.gsb_mobile_app;
 
-public class ManageExpenseSheet {
+import android.app.ProgressDialog;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class ManageExpenseSheet extends Fragment {
+    private static final String TAG = "ManageExpenseSheet";
+    ProgressDialog progressDialog;
+    RequestQueue requestQueue;
+    private static final String EXTRA_MESSAGE = "com.example.gsb_mobile_app.extra.MESSAGE";
+    public static ManageExpenseSheet newInstance(String message) {
+        ManageExpenseSheet fragment = new ManageExpenseSheet();
+        Bundle args = new Bundle();
+
+        args.putString(EXTRA_MESSAGE, message);
+        fragment.setArguments(args);
+
+        return fragment;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_manage_expense_sheet, container, false);
+
+        progressDialog = new ProgressDialog(getActivity());
+        requestQueue = Volley.newRequestQueue(getActivity());
+
+        progressDialog.setMessage("Récupération des données...");
+        progressDialog.setCancelable(false);
+
+        return view;
+    }
 }
--- app/src/main/java/com/example/gsb_mobile_app/VisitorPortal.java
+++ app/src/main/java/com/example/gsb_mobile_app/VisitorPortal.java
@@ -8,16 +8,21 @@ import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import java.util.HashMap;
-import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class VisitorPortal extends Fragment {
     private static final String TAG = "VisitorPortal";
@@ -62,18 +67,22 @@ public class VisitorPortal extends Fragment {
 
                     for (int i = 0; i < result.length(); i++) {
                         JSONObject data = result.getJSONObject(i);
-                        View expenseSheet = inflater.inflate(R.layout.expense_sheet, expenseSheetsList, false);
-                        TextView RequestDateTextView = expenseSheet.findViewById(R.id.requestDateTextView);
-                        TextView nightsNumberTextView = expenseSheet.findViewById(R.id.nightsNumberTextView);
-                        TextView totalAmountTextView = expenseSheet.findViewById(R.id.totalAmountTextView);
-                        TextView treatmentStatusTextView = expenseSheet.findViewById(R.id.treatmentStatusTextView);
+                        View expenseSheetItem = inflater.inflate(R.layout.expense_sheet, expenseSheetsList, false);
+                        LinearLayout expenseSheet = expenseSheetItem.findViewById(R.id.expenseSheet);
+                        TextView RequestDateTextView = expenseSheetItem.findViewById(R.id.requestDateTextView);
+                        TextView nightsNumberTextView = expenseSheetItem.findViewById(R.id.nightsNumberTextView);
+                        TextView totalAmountTextView = expenseSheetItem.findViewById(R.id.totalAmountTextView);
+                        TextView treatmentStatusTextView = expenseSheetItem.findViewById(R.id.treatmentStatusTextView);
 
                         RequestDateTextView.setText(data.optString("request_date"));
                         nightsNumberTextView.setText(data.optString("nights_number"));
                         totalAmountTextView.setText(data.optString("total_amount"));
                         treatmentStatusTextView.setText(data.optString("treatment_status"));
 
-                        expenseSheetsList.addView(expenseSheet);
+                        final String expenseSheetId = data.optString("expense_sheet_id");
+                        expenseSheet.setOnClickListener(v -> manageExpenseSheetRequest(expenseSheetId));
+
+                        expenseSheetsList.addView(expenseSheetItem);
                     }
                 } else {
                     Toast.makeText(getActivity(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
@@ -98,6 +107,44 @@ public class VisitorPortal extends Fragment {
         requestQueue.add(stringRequest);
         progressDialog.show();
     }
-}
+    private void manageExpenseSheetRequest(String expenseSheetId) {
+        String gettingExpenseSheetsURL = "https://jeremiebayon.fr/api/controllers/functionalities/ExpenseSheet/ManageExpenseSheet.php";
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, gettingExpenseSheetsURL, response -> {
+            progressDialog.dismiss();
 
+            try {
+                JSONObject jsonObject = new JSONObject(response);
+                int status = jsonObject.getInt("status");
+
+                if (status == 200) {
+                    Toast.makeText(getActivity(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                    ManageExpenseSheet fragment = ManageExpenseSheet.newInstance(jsonObject.getString("data"));
+                    FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
+                    transaction.replace(R.id.frameLayout, fragment);
+                    transaction.addToBackStack(null);
+                    transaction.commit();
+                } else {
+                    Toast.makeText(getActivity(), jsonObject.getString("message"), Toast.LENGTH_LONG).show();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                Toast.makeText(getActivity(), "Un problème est survenu.", Toast.LENGTH_LONG).show();
+            }
+        },
+                error -> {
+                    progressDialog.dismiss();
+                    Toast.makeText(getActivity(), "Échec de la connexion au serveur.", Toast.LENGTH_LONG).show();
+                }) {
+            @Override
+            protected Map<String, String> getParams() {
+                Map<String, String> params = new HashMap<>();
+                params.put("id", expenseSheetId);
+                return params;
+            }
+        };
+
+        requestQueue.add(stringRequest);
+        progressDialog.show();
+    }
+}
 
